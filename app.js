b9bb67ac1010874062a55ecf29f7ab6f3cc136ee
Neil Pretorius <neil@kva.co.za>
2020-05-16 00:17:09 +0200

    Added style.css with adding lists

--- app.js
+++ app.js
@@ -5,26 +5,32 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let listItems = [];
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 	console.log("Hello");
 	//Todo: res.sendFile(__dirname + "/index.html");
 	var today = new Date();
+	var options = {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+	};
 
-	var weekday = new Array(7);
-	weekday[0] = "Sunday";
-	weekday[1] = "Monday";
-	weekday[2] = "Tuesday";
-	weekday[3] = "Wednesday";
-	weekday[4] = "Thursday";
-	weekday[5] = "Friday";
-	weekday[6] = "Saturday";
+	var day = today.toLocaleDateString("en-US", options);
 
-	var currentDay = today.getDay();
-	var day = weekday[currentDay];
+	res.render("list", { kindOfDay: day, newListitems: listItems });
+});
 
-	res.render("list", { kindOfDay: day });
+app.post("/", function (req, res) {
+	let listItem = req.body.newItem;
+	listItems.push(listItem);
+	res.redirect("/");
+	console.log(listItem);
 });
 
 app.listen(process.env.PORT || 3000, function () {
